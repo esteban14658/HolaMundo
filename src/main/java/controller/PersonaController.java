0f73ddd208a6156ae0c56955efda69220d7e0641
tupapiriki27 <jimmyapaez@ucundinamarca.edu.co>
2020-10-15 21:18:14 -0500

    Se crea controlador para persona PersonaController y se le asignan su respectivo constructor y metodos.

--- src/main/java/controller/PersonaController.java
+++ src/main/java/controller/PersonaController.java
@@ -5,10 +5,46 @@
  */
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import model.PersonaModel;
+import view.PersonaView;
+
 /**
  *
  * @author Jimmy Alexander Paez Moreno
  */
 public class PersonaController {
+    private PersonaModel personaModel;
+    private PersonaView personaView;
+
+    public PersonaController(PersonaModel personaModel, PersonaView personaView) {
+        this.personaModel = personaModel;
+        this.personaView = personaView;
+        
+        this.personaView.agregarBotonListener(new AgregarBotonListener());
+    }
     
+    class AgregarBotonListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String nombre, apellido;
+            
+            try{
+                nombre = personaView.getTFNombre();
+                apellido = personaView.getTFApellido();
+                
+                PersonaModel persona = new PersonaModel(nombre,apellido);
+                
+                personaModel.agregarPersona(persona);
+                
+                personaView.setTableModelPersonas(personaModel.getListaPersona());
+            }catch(Exception ex){
+                personaView.mostrarError("Ambos campos deben estar llenos.");
+                System.out.println(ex.getMessage());
+            }
+        }
+        
+    }
 }
