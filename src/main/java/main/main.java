cbfbb149ac9db3a54077e15c7303f51f7341f729
tupapiriki27 <jimmyapaez@ucundinamarca.edu.co>
2020-10-15 21:43:45 -0500

    Terminado codigo empezando pruebas

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -5,10 +5,21 @@
  */
 package main;
 
+import controller.PersonaController;
+import model.PersonaModel;
+import view.PersonaView;
+
 /**
  *
  * @author Jimmy Alexander Paez Moreno & Esteban Rodrigo Beltran Abello
  */
 public class main {
-    
+    public static void main(String args[]) {
+        PersonaModel modelo = new PersonaModel();
+        PersonaView vista = new PersonaView();
+        
+        PersonaController controlador = new PersonaController(modelo, vista);
+        
+        vista.setVisible(true);
+    }
 }
