b021376570853a737061650d966ed61cde869498
tupapiriki27 <jimmyapaez@ucundinamarca.edu.co>
2020-10-15 19:08:10 -0500

    Se crean las propiedades nombre y apellido. Se simula una base de datos con un arreglo de personas. Se crean getters y setters de nombre y apellido. Se crean getters para el arreglo de personas y se añade metodo para agregar personas a este arreglo.

--- src/main/java/model/Persona.java
+++ src/main/java/model/Persona.java
@@ -5,10 +5,44 @@
  */
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Jimmy Alexander Paez Moreno
  */
 public class Persona {
-    
+    private final static List<Persona> listaPersona = new ArrayList();
+    private String nombre;
+    private String apellido;
+
+    public Persona(String nombre, String apellido) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public static List<Persona> getListaPersona() {
+        return listaPersona;
+    }
+
+    public void agregarPersona(Persona persona) {
+        Persona.listaPersona.add(persona);
+    }
 }
